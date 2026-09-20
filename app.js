a961a089260df01f530b3fa14e776386c96ce74e
dothecoolwip1 <160705346+dothecoolwip1@users.noreply.github.com>
2026-09-20 16:55:35 -0600

    Force Reavers service worker updates

--- app.js
+++ app.js
@@ -157,6 +157,6 @@
   });
 
   if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => navigator.serviceWorker.register("sw.js").catch(() => {}));
+    window.addEventListener("load", () => navigator.serviceWorker.register("sw.js?v=10", { updateViaCache: "none" }).then(registration => registration.update()).catch(() => {}));
   }
 })();
