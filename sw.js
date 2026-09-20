a111f31eade39c951ca84189f44070600fe288a0
dothecoolwip1 <160705346+dothecoolwip1@users.noreply.github.com>
2026-09-20 15:50:33 -0600

    Refresh Reavers site cache for admin managed content

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="reavers-team-hub-v4";
-const SHELL=["./","./index.html","./team.html","./training.html","./events.html","./media.html","./styles.css","./team-data.js","./app.js","./manifest.webmanifest","./assets/reavers-mark.svg"];
+const CACHE="reavers-team-hub-v5";
+const SHELL=["./","./index.html","./team.html","./training.html","./events.html","./media.html","./styles.css","./dynamic.css","./team-data.js","./app.js","./manifest.webmanifest","./assets/reavers-mark.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener("fetch",event=>{
